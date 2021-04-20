c7931a1b5effd72d02193bf40445c03383a0df71
Addis Belete <addis.bel@gmail.com>
2021-04-20 12:24:13 +0300

    Add actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,30 @@
 class UsersController < ApplicationController
-    def new
+  def index
+    @user = User.all
+  end
 
-    end
-    def create
+  def show
+    @user = User.find(params[:id])
+  end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Your account created successfully"
+      redirect_to @user
+    else
+      render :new
+      flash[:alert] = "Create your account"
     end
-    def show
-        
-    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
 end
